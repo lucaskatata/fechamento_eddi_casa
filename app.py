08bf1d929b45e365ea1bc2268cfb46f0acd9434c
lucaskatata <104213451+lucaskatata@users.noreply.github.com>
2025-07-18 15:22:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def find_closest_sku(sku, sku_list):
     return matches[0] if matches else None
 
 
+st.set_page_config(layout="wide")
 st.title("Fechamento - Mão de Obra")
 
 url_valores = "https://docs.google.com/spreadsheets/d/1JkafGyVeOQjCvMmePSfrgW3rSrTs6bzcL01G1Spge4s/export?format=csv&gid=2104680401#gid=2104680401"
